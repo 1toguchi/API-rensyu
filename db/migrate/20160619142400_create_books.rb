1ab93636089d539a5b21300a9086e59d2cc21329
熊野朝仁 <kmntmht116@gmail.com>
2016-06-28 15:38:17 +0900

    test

--- db/migrate/20160619142400_create_books.rb
+++ db/migrate/20160619142400_create_books.rb
@@ -1,8 +1,12 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-
-      t.timestamps null: false
+    
+    t.string :title, null: false
+    t.text :detail
+    t.binary :image
+    
+    t.timestamps null: false
     end
   end
 end
